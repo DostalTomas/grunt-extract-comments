0f9b2480dd766e3a5ffcec0eb3bccabbaa13919b
Tomas Dostal <Dostal.Tomas@gmail.com>
2019-02-12 14:51:12 +0100

    Add grunt-extract-comments

---
+++ README.md
@@ -0,0 +1,9 @@
+Grunt task for extracting comments from file
+
+```javascript
+grunt.initConfig({
+    'extract-comments': {
+        'dist/docs/comments.js': ['src/**/*.ts']
+    }
+})
+```
---
+++ tasks/extract-comments.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = (grunt) => {
+
+    const extract = require('extract-comments');
+
+    grunt.registerMultiTask('extract-comments', 'Get all comments from files and save them to separate file.', () => {
+        this.files.forEach((file) => {
+            const src = file.src.filter((filepath) => {
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map((filepath) => {
+                const content = grunt.file.read(filepath);
+                const commentsAst = extract(content);
+
+                return commentsAst.map((comment) => `/*${comment.raw}*/`).join('\n');
+            });
+
+            grunt.file.write(file.dest, src.join(''), {encoding: 'utf-8'});
+
+            grunt.log.writeln('File "' + file.dest + '" created.');
+        });
+    });
+
+};
