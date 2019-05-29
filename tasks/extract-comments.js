6ee3078112705359306973c8766cf118b60a8e9a
Tomas Dostal <Dostal.Tomas@gmail.com>
2019-05-29 17:22:35 +0200

    v1.1.0 - Change extractor to @babel/parser

--- tasks/extract-comments.js
+++ tasks/extract-comments.js
@@ -2,22 +2,51 @@
 
 module.exports = (grunt) => {
 
-    const extract = require('extract-comments');
+    const parser = require('@babel/parser');
 
-    grunt.registerMultiTask('extract-comments', 'Get all comments from files and save them to separate file.', function() {
+    grunt.registerMultiTask('extract-comments', 'Get all comments from files and save them to separate file.', function () {
         this.files.forEach((file) => {
-            const src = file.src.filter((filepath) => {
+            let src = file.src.filter((filepath) => {
                 if (!grunt.file.exists(filepath)) {
                     grunt.log.warn('Source file "' + filepath + '" not found.');
                     return false;
                 } else {
                     return true;
                 }
-            }).map((filepath) => {
-                const content = grunt.file.read(filepath);
-                const commentsAst = extract(content);
+            });
 
-                return commentsAst.map((comment) => `/*${comment.raw}*/`).join('\n');
+            src = src.map((filepath) => {
+                const content = grunt.file.read(filepath);
+
+                const commentsAst = parser.parse(content, {
+                    sourceType: 'module',
+                    plugins: [
+                        'typescript',
+                        'asyncGenerators',
+                        'bigInt',
+                        'classProperties',
+                        'classPrivateProperties',
+                        'classPrivateMethods',
+                        'decorators-legacy',
+                        'doExpressions',
+                        'dynamicImport',
+                        'exportDefaultFrom',
+                        'exportNamespaceFrom',
+                        'functionBind',
+                        'functionSent',
+                        'importMeta',
+                        'logicalAssignment',
+                        'nullishCoalescingOperator',
+                        'numericSeparator',
+                        'objectRestSpread',
+                        'optionalCatchBinding',
+                        'optionalChaining',
+                        'partialApplication',
+                        'throwExpressions'
+                    ]
+                });
+
+                return commentsAst.comments.map((c) => `/*${c.value}*/`).join('\n');
             });
 
             grunt.file.write(file.dest, src.join(''), {encoding: 'utf-8'});
